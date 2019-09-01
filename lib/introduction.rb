796787956e0b52053514cfc88a99bf96d0558a3b
Parker Bacall <pbacall2@gmail.com>
2019-09-01 21:40:54 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,11 +5,17 @@
 
 
 
+def introduction(name)
+  put "Hi, my name is #{name}."
+end
 
+def introduction_with_language(name, language)
+  put "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-
-
-
+def introduction_with_language(name, language = "Ruby")
+  put "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
